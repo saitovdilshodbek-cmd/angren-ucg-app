bb92ce5101bb4ad43d10a7c7234305eec58de81c
saitovdilshodbek-cmd <saitovdilshodbek@gmail.com>
2026-04-07 15:44:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-import streamlit as st
-import qrcode
-from io import BytesIO
 
-st.title("QR test")
-url = "https://example.com"
-qr = qrcode.QRCode(box_size=5)
-qr.add_data(url)
-qr.make()
-img = qr.make_image()
-buf = BytesIO()
-img.save(buf, format="PNG")
-st.image(buf.getvalue())
