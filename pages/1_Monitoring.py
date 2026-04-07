d8fffa760474738c261a4af952ae589ce9e462c1
saitovdilshodbek-cmd <saitovdilshodbek@gmail.com>
2026-04-07 23:35:25 +0300

    Update 1_Monitoring.py

--- pages/1_Monitoring.py
+++ pages/1_Monitoring.py
@@ -1,4 +1,1 @@
-import streamlit as st
-st.set_page_config(page_title="Live Monitoring", layout="wide")
-st.header("🔴 LIVE UCG MONITORING")
-# Sizning real-time monitoring kodingiz (app.py dagi live monitoring qismini ko'chirish)
+
