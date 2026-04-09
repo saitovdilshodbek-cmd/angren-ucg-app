30cb15a74a32595148d9ceb4c5f2f8287d4f17fc
saitovdilshodbek-cmd <saitovdilshodbek@gmail.com>
2026-04-09 23:58:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -486,8 +486,8 @@ url = "https://angren-ucg-app-a7rxktm6usxqixabhaq576.streamlit.app/#ucg-termo-me
 def generate_qr(link):
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
     qr.add_data(link)
-    qr.make(fit)
-        img = qr.make_image(fill_color="black", back_color="white")
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
     buf = BytesIO()
     img.save(buf, format="PNG")
     return buf.getvalue()
